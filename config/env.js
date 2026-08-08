cfda5f4bcff341fd886df72d1fe8ee7bc0481bf0
Abhishek Sharma <as684844@gmail.com>
2026-08-08 18:21:23 +0530

    feat: implement centralized environment configuration with dotenv and add nodemon development script

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/docpa",
   
   // Access Token Config
